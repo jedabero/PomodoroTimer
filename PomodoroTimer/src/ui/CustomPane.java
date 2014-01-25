23c4934d4353cc817a3ec36aa1ca89d59eceabe7
jedabero <jedabero@hotmail.com>
2014-01-25 11:02:29 -0500

    Added CustomPane class for future ui stuff.

---
+++ PomodoroTimer/src/ui/CustomPane.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ui;
+
+import javax.swing.JPanel;
+
+/**
+ * @author Jedabero
+ *
+ */
+public class CustomPane extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6034649606926819570L;
+
+}
